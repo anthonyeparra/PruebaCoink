a32fae4eeb619130f66817d23f071aa17b3c7757
Aparra <anthonyeparra@hotmail.com>
2022-08-26 08:18:51 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,68 @@
+﻿using ApiCoinkData.Repositories;
+using ApiCoinkModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace PruebaCoink
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRespository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRespository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> getAllUser()
+        {
+            try
+            {
+
+                return Ok(await _usuarioRepository.GetUsuarios());
+            }
+            catch (Exception ex)
+            {
+
+                return Ok(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateNewUser")]
+        public async Task<IActionResult> CreateUsuario([FromBody] Usuario user)
+        {
+            if (user == null) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var created = await _usuarioRepository.InsertUsuario(user);
+
+            return Created("created", created);
+        }
+        [HttpPost]
+        [Route("CreateNewUserSp")]
+        public async Task<IActionResult> CreateUsuarioProcedure([FromBody] Usuario user)
+        {
+            try
+            {
+                if (user == null) return BadRequest();
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+
+                var created = await _usuarioRepository.InsertStoredProcedureUsuario(user);
+
+                return Created("created", created);
+
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+     
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using ApiCoinkData;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCoinkData.Repositories;
+using ApiCoinkModel;
+
+namespace PruebaCoink
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Obtengo cadena de conexion 
+            var mySQLConectionConfig = new MySQLConfiguration(Configuration.GetConnectionString("MysqlConnection"));
+            // Lo inyecto
+            services.AddSingleton(mySQLConectionConfig);
+            services.AddScoped< IUsuarioRespository, UsuarioRepository > ();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
